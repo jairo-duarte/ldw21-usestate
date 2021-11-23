6fec3f9d2c774ef25322c4f4d49e1b9f810f4d66
Jairo Duarte <jairo.duarte@exacta.inf.puc-rio.br>
2021-11-23 18:42:53 -0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [counter,setCounter]  =  useState(1);
+  const initCountObj = {contador:1};
+  const [countObj,setCountObj]  =  useState(initCountObj);
+  const exibir_reset = counter > 10;
+  
+  const reset = () => {
+    setCounter(0);
+  }
+  
+
+  
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">     
+        counter={counter}
+        <br/>
+        counter2={countObj.contador}
+        <button onClick={() => {
+          counter++
+          setCounter(counter); 
+          countObj.contador++ ;
+          setCountObj(countObj);
+          console.log('teste')
+          }}> incrementar counter </button>
+        {exibir_reset?<button onClick={reset}> reset </button>:''}
       </header>
     </div>
   );
