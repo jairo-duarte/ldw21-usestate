4d9d56fa3108fea7d27b035541fbb5b22ec43190
Jairo Duarte <jairo.duarte@exacta.inf.puc-rio.br>
2021-11-23 21:04:38 -0300

    rename , console log

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  
   let [counter,setCounter]  =  useState(1);
   const initCountObj = {contador:1};
   const [countObj,setCountObj]  =  useState(initCountObj);
+
   const exibir_reset = counter > 10;
   
   const reset = () => {
     setCounter(0);
   }
   
-
-  
-
  
   return (
     <div className="App">
       <header className="App-header">     
-        counter={counter}
+        counter={counter} 
         <br/>
-        counter2={countObj.contador}
+        counterObject={countObj.contador}
         <button onClick={() => {
-          counter++
+          counter++       
           setCounter(counter); 
+          
           countObj.contador++ ;
-          setCountObj(countObj);
-          console.log('teste')
+          
+          setCountObj({...countObj});
+          console.log('onClick executado , counter=' + counter +  ' counterObj=' + countObj.contador);
           }}> incrementar counter </button>
         {exibir_reset?<button onClick={reset}> reset </button>:''}
       </header>
